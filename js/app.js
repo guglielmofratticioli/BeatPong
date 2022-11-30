02f9d92e2607471a46e224be419d1c0ebc47ce83
Guglielmo Fratticioli <guglielmo.fratticioli@gmail.com>
2022-11-30 17:41:10 +0100

    gagag

--- js/app.js
+++ js/app.js
@@ -78,7 +78,7 @@ function render(){
     $.fillStyle = "black"; 
     $.fillRect(paddle.x,paddle.y,paddle.width,paddle.height);
 }
-_game.addEventListener("resize", render(););
+_game.on("resize", function(){ render(); });
 
 function setCanvasSize(canvas) {
     var parent = canvas.parentNode,
